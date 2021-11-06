33b2a706c8a60f25a791bd5efc8cc6f3ecaad321
nick <smz1680@gmail.com>
2021-11-06 10:29:34 -0700

    pre-seed database data

---
+++ database_schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(28),
+    passowrd TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL CHECK (position('@' IN email) > 1),
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    avatar_url TEXT NOT  NULL DEFAULT 'local'
+);
+
+CREATE TABLE counties (
+    id SERIAL PRIMARY KEY,
+    county_name TEXT NOT NULL,
+    state TEXT NOT NULL
+);
+
+CREATE TABLE cities (
+    id SERIAL PRIMARY KEY,
+    city TEXT NOT NULL,
+    county INTEGER REFERENCES counties ON DELETE CASCADE
+);
+
+CREATE TABLE dog_place_type (
+    place_type VARCHAR(25) PRIMARY KEY
+);
+
+CREATE TABLE dog_place_detail (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    address TEXT NOT NULL,
+    description TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    city INTEGER REFERENCES cities ON DELETE CASCADE,
+    place_type TEXT REFERENCES dog_place_type on DELETE CASCADE,
+    zipcode TEXT NOT NULL
+);
+
+CREATE TABLE review_comments (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users ON DELETE CASCADE,
+    dog_place_id INTEGER REFERENCES dog_place_detail ON DELETE CASCADE,
+    score INTEGER NOT NULL,
+    comment TEXT NOT NULL
+);
---
+++ park_hospital.sql
@@ -0,0 +1,24 @@
+-- delete existing datbase, and create a new database
+
+DROP DATABASE IF EXISTS animal_parts_hospitals;
+CREATE DATABASE animal_parts_hospitals;
+
+-- connect to that database
+\connect animal_parts_hospitals
+
+-- create database tables
+\i database_schema.sql
+
+-- seed parks and hospitals datas
+\i database_data-seed.sql 
+
+-- delete testing database if exist or create testing database
+DROP DATABASE IF EXISTS animal_parts_hospitals_test;
+CREATE DATABASE animal_parts_hospitals_test;
+
+-- connect to testing database
+\connect animal_parts_hospitals_test
+
+-- create database tables
+\i database_schema.sql
+
